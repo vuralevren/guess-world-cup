b2eda8a9ca96f325b5b1cd2e5f2e0ed1462068db
vuralevren <evren@altogic.com>
2022-11-23 16:21:03 +0300

    Cursor bug fixed

--- src/components/template.js
+++ src/components/template.js
@@ -239,7 +239,7 @@ export default function Sidebar({ children }) {
                             }
                             className={cs(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
                             )}
                           >
                             Sign Out

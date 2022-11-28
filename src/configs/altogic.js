ba56ca9e4df9f4dd9f4ce0c947c5e5b46892a493
vuralevren <evren@altogic.com>
2022-11-28 14:18:49 +0300

    Prod config

--- src/configs/altogic.js
+++ src/configs/altogic.js
@@ -1,7 +1,7 @@
 import { createClient } from "altogic";
 
 // This `envUrl` and `clientKey` is sample you need to create your own.
-const envUrl = "https://guess-world-cup.c1-europe.altogic.com";
+const envUrl = "https://world-cup.c1-europe.altogic.com";
 const clientKey = "853f29ed55064e8ba583cdbdfe83aadf";
 
 const altogic = createClient(envUrl, clientKey, {
